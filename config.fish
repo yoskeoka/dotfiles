d8199122dc61513c7e8d5879d080c8d37373de5f
yoskeoka <y.okamoto@gemcook.com>
2019-01-22 02:11:08 +0900

    [update] fisher add

--- config.fish
+++ config.fish
@@ -10,7 +10,7 @@ set -x PATH $HOME/.cargo/bin $PATH
 # add python user base to PATH
 set USER_BASE_PATH (python -m site --user-base)
 set -x PATH $PATH {$USER_BASE_PATH}/bin
- 
+
 set -x EDITOR vim
 
 # superior /usr/local/bin than /usr/bin
@@ -21,7 +21,7 @@ set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 # use gnu version unix commands
 set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -x  MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
 alias awk='gawk'
 alias factor='gfactor'
@@ -33,46 +33,50 @@ alias open.='open .'
 bass (dircolors ~/.colorrc)
 
 function history-merge --on-event fish_preexec
-  history --save
-  history --merge
+    history --save
+    history --merge
 end
 
 function peco_sync_select_history
-  history-merge
-  peco_select_history $argv
+    history-merge
+    peco_select_history $argv
 end
 
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 function __ghq_cd_repository -d "Change local repository directory"
-  ghq list --full-path | fzf --reverse --ansi | read -l repo_path
-  if echo $repo_path | grep -E "/" >/dev/null ^/dev/null
-    cd $repo_path
-    commandline -f repaint
-  end
+    ghq list --full-path | fzf --reverse --ansi | read -l repo_path
+    if echo $repo_path | grep -E "/" >/dev/null ^/dev/null
+        cd $repo_path
+        commandline -f repaint
+    end
 end
 
 function fish_user_key_bindings
-  bind \cr 'peco_sync_select_history (commandline -b)'
-#   bind \cs peco_select_ghq_repository
-  bind \cg '__ghq_cd_repository'
-  if bind -M insert >/dev/null ^/dev/null
-    bind -M insert \cg '__ghq_cd_repository'
-  end
+    bind \cr 'peco_sync_select_history (commandline -b)'
+    #   bind \cs peco_select_ghq_repository
+    bind \cg '__ghq_cd_repository'
+    if bind -M insert >/dev/null ^/dev/null
+        bind -M insert \cg '__ghq_cd_repository'
+    end
 end
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f ~/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish ]; and source ~/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish
+[ -f ~/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish ]
+and source ~/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish
 # tabtab source for sls package
 # uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f ~/.config/yarn/global/node_modules/tabtab/.completions/sls.fish ]; and source ~/.config/yarn/global/node_modules/tabtab/.completions/sls.fish
+[ -f ~/.config/yarn/global/node_modules/tabtab/.completions/sls.fish ]
+and source ~/.config/yarn/global/node_modules/tabtab/.completions/sls.fish
 
-test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+test -x (which aws_completer)
+and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 set -x DOCKER_BUILDKIT 1
 
-[ -f ~/.google-cloud-sdk/path.fish.inc ]; and source ~/.google-cloud-sdk/path.fish.inc
+[ -f ~/.google-cloud-sdk/path.fish.inc ]
+and source ~/.google-cloud-sdk/path.fish.inc
 
 set -x HOMEBREW_INSTALL_CLEANUP 1
 
--- lib/fisher.fish
+++ lib/fisher.fish
@@ -2,17 +2,18 @@
 
 # https://github.com/jorgebucaran/fisher
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    set -q XDG_CONFIG_HOME
+    or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
 if functions -q fisher
-  fisher install \
-    bobthefish \
-    balias \
-    bass \
-    bd \
-    peco \
-    peco_select_ghq_repository
+    fisher add \
+        oh-my-fish/theme-bobthefish \
+        oh-my-fish/plugin-balias \
+        edc/bass \
+        0rax/fish-bd \
+        oh-my-fish/plugin-peco \
+        yoshiori/fish-peco_select_ghq_repository
 end
